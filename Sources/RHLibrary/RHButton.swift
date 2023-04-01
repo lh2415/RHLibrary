fa102d9fa1483e4622a268525ada624b218140c0
lh2415 <663521713@qq.com>
2023-04-01 15:52:08 +0800

    TestFile was added

---
+++ Sources/RHLibrary/RHButton.swift
@@ -0,0 +1,20 @@
+//
+//  RHButton.swift
+//  
+//
+//  Created by Run Hua on 2023/3/30.
+//
+
+import UIKit
+
+class RHButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Sources/RHLibrary/RHLabel.swift
@@ -0,0 +1,20 @@
+//
+//  RHLabel.swift
+//  
+//
+//  Created by Run Hua on 2023/4/1.
+//
+
+import UIKit
+
+class RHLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
